39bd3255dccdcaf343e3df86be0a237a424955e1
Bruno Lusa <bruno.lusa@gmail.com>
2024-07-18 23:36:18 -0300

    Inserindo primeiros arquivos com esboco do projeto e primeiro teste parcialmente implementado

---
+++ mobile-testing-sample/UnitTest1.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using System;
+
+namespace mobile_testing_sample
+{
+    public class Tests
+    {
+        private AndroidDriver driver;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var serverUri = new Uri(Environment.GetEnvironmentVariable("APPIUM_HOST") ?? "http://127.0.0.1:4723/");
+            var driverOptions = new AppiumOptions()
+            {
+                AutomationName = AutomationName.AndroidUIAutomator2,
+                PlatformName = "Android",
+                DeviceName = "Android Emulator",
+            };
+
+            driverOptions.AddAdditionalAppiumOption("appPackage", "com.android.settings");
+            driverOptions.AddAdditionalAppiumOption("appActivity", ".Settings");
+            // NoReset assumes the app com.google.android is preinstalled on the emulator
+            driverOptions.AddAdditionalAppiumOption("noReset", true);
+
+            driver = new AndroidDriver(serverUri, driverOptions, TimeSpan.FromSeconds(180));
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            driver.Dispose();
+        }
+
+        [Test]
+        public void SetOnOffWifiNetWork()
+        {
+
+            driver.ToggleWifi();
+           
+        }
+    }
+}
